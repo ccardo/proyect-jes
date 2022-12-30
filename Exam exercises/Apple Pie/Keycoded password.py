d7504a1cfbd714df4c3b51b57adcece414398178
ccardo <riccardo.gallo.pb+github@gmail.com>
2022-12-30 21:49:04 +0100

    changed & optimized the ApplePieRecipe code.

--- Exam exercises/Apple Pie/Keycoded password.py
+++ Exam exercises/Apple Pie/Keycoded password.py
@@ -1,43 +1,26 @@
 ##
 #
 
-def get_instructions():
-
-    with open("ApplePieRecipe.txt", "r") as file:
-        lines = [i.strip() for i in file.readlines()]
-
-    instr = list(filter(lambda line: lines.index(line) > lines.index("Instructions"), lines))
-    instr = list(map(lambda x: x.split(), instr))
-    instructions = list()
-    for i in instr:
-        instructions.extend(i)
-
-    return instructions
-
-
-def decode():
-
-    with open("keyCode.txt", "r") as file:
-        key = file.readline().strip()
-
-    word = list()
-    text = get_instructions()
-    for i in key:
-        letters = list(filter(lambda x: i in x and len(x) > 3 and x[0] != i, text))
-        word.append(list(map(lambda x: x[0], letters)))
-
-    return word
-
-
 def main():
 
-    wordList = decode()
-    decoded = ""
-    for i in wordList:
-        for j in i:
-            decoded += j
+    with open("ApplePieRecipe.txt", "r") as recipeFile:
+        recipeLines = [i.strip().split() for i in recipeFile.readlines()]
 
-    print(decoded)
+    # save all lines in the instructions part --> index of the liene > index of "Instructions"
+    importantLines = list()
+    for i, line in enumerate(recipeLines):
+        if i > recipeLines.index(["Instructions"]):
+            importantLines.extend(line)
+
+    with open("keyCode.txt", "r") as keyCode:
+        keys = keyCode.readline().strip()
+
+    for key in keys:
+
+        # apply the decoding criteria: word longer than 3 characters, first letter not the key letter
+        for word in importantLines:
+            if key in word and len(word) > 3 and word[0] != key:
+                print(word[0], end="")
 
 
 if __name__ == '__main__':

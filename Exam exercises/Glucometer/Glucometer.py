cbfeca34038d31abd74d7e6346e1c38d3d0765c0
ccardo <riccardo.gallo.pb+github@gmail.com>
2023-01-05 20:48:05 +0100

    Finished Glucometer.py in the Exam exercises folder.

--- Exam exercises/Glucometer/Glucometer.py
+++ Exam exercises/Glucometer/Glucometer.py
@@ -3,31 +3,38 @@
 
 def main():
 
-    file_path = "Exam exercises/Glucometer/values.txt"
+    # open and read the file
+    file_path = "values.txt"
     with open(file_path, "r") as file:
         lines = [i.split() for i in file]
-    
-    print(*lines, sep="\n")
 
+    # pick the instances where the blood sugar > 200
     max_value = 200
-    exceedances = {}
+    patients_at_risk = {}
     for patient in lines:
 
         code = patient[0]
-        sugar_value = int(patient[2])
+        sugar_value = patient[2]
 
-        if sugar_value > max_value:
+        if int(sugar_value) >= max_value:
 
-            if code not in exceedances:
-                exceedances[code] = {int(sugar_value)}
+            if code not in patients_at_risk:
+                patients_at_risk[code] = [sugar_value]
             else:
-                exceedances[code].add(sugar_value)
+                patients_at_risk[code].append(sugar_value)
 
-    for patient in lines:
+    # sort the excesses by the number of instances in which they occurred
+    sorted_by_criticality = sorted(patients_at_risk.items(), key=lambda x: len(x[1]), reverse=True)
 
-        if any(x[2] in exceedances[x[0]] for x in lines):
+    # print the desired patient info
+    for patient in sorted_by_criticality:
+        print()
+        for patient_info in lines:
 
-            print(patient[0], patient[1], patient[2])
+            if patient[0] == patient_info[0] and patient_info[2] in patient[1]:
+
+                print(f"{patient_info[0]} {patient_info[1]} {patient_info[2]}")
 
 
-main()
+if __name__ == '__main__':
+    main()

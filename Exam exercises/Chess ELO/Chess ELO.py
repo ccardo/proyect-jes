4f57a95b799f5468caa05b31bdebe538bc6b6219
ccardo <riccardo.gallo.pb+github@gmail.com>
2023-01-09 19:22:26 +0100

    added games_short.csv, updated Chess ELO.py in the Chess Elo folder.

--- Exam exercises/Chess ELO/Chess ELO.py
+++ Exam exercises/Chess ELO/Chess ELO.py
@@ -9,38 +9,28 @@ def main():
 
     from csv import reader
 
-    players = {}
     with open("players.csv") as file:
-        data = [i.split(",") for i in file.readlines()]
-        for line in data[1:]:
-            players.update({line[0].strip(): int(line[1].strip())})         # create dict with name and ELO
+        data = [i.split(",") for i in file.readlines()][1:]
+        players = {line[0]: int(line[1]) for line in data}                  # create dict with name and ELO
 
-    games = list(reader(open("games.csv")))[1:]
-    for game in games:
-        game.extend(game.pop(2).split("-"))                                 # split the score at the dash
+    games = list(reader(open("games.csv")))[1:]                             # split the score at the dash
 
     for game in games:
 
-        for player in [game[0], game[1]]:                                   # update "players" with 1500 as ELO
+        first_player, second_player = game[:2]
+        for player in [first_player, second_player]:                        # update "players" with 1500 as ELO
             if player not in players:                                       # if the player is not in players
                 players.update({player: 1500})
 
-        elo0 = players[game[0]]
-        elo1 = players[game[1]]
-        elo_difference = 200 * round(delta(elo0, elo1))
+        elo_first = players[first_player]
+        elo_second = players[second_player]
+        elo_difference = round(200 * delta(elo_first, elo_second))
 
-        try:
-            if int(game[2]) > int(game[3]):                                 # sees who won:
-                elo0 = elo_difference                                       # except ValueError --> if the score
-                elo1 = -elo_difference                                      # is 1/2 (tie), it raises ValueError
-            elif int(game[2]) < int(game[3]):                               # and does nothing to the ELOs
-                elo0 = -elo_difference
-                elo1 = elo_difference
-        except ValueError:
-            continue
+        elo0 = elo_difference if game[2] == "1-0" else -elo_difference
+        elo1 = -elo0
 
-        players.update({game[0]: players.get(game[0]) + elo0})
-        players.update({game[1]: players.get(game[1]) + elo1})
+        players[game[0]] += elo0
+        players[game[1]] += elo1
 
     # print the ELOs in descending order
     print(*sorted(players.items(), key=lambda x: x[1], reverse=True), sep="\n")

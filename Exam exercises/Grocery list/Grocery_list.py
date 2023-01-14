e2a49c2d6a063912c2a39a6b0220093f5192aefe
ccardo <riccardo.gallo.pb+github@gmail.com>
2023-01-14 22:00:34 +0100

    gg

--- Exam exercises/Grocery list/Grocery_list.py
+++ Exam exercises/Grocery list/Grocery_list.py
@@ -15,20 +15,29 @@ def main():
 
     receipt = get_list("receipt.csv")
     roommate_list = get_list("roommate_list.csv", roommate_list=True)
+    total = sum([int(receipt[item][1]) for item in receipt])
 
     print("\nItems not bought by the roommate:")
     due_money: float = 0.0
     for item, count in roommate_list.items():
 
+        missing_item_count: float = 0.0
         try:
             item_count_in_receipt = receipt[item][0]
             total_price_in_receipt = receipt[item][1]
-            due_money += total_price_in_receipt / item_count_in_receipt * count
+            
+            if item_count_in_receipt < count:
+                missing_item_count = count - item_count_in_receipt
+                due_money += total_price_in_receipt
+                print(f"\t* {item} - {missing_item_count}")
+            else:
+                due_money += total_price_in_receipt / item_count_in_receipt * count
 
         except KeyError:
-            print(f"\t * {item};")
+            print(f"\t* {item} - {count};")
 
     print(f"\nDue money: {due_money} $")
+    print(f"Total expense: {total} $")
 
 
 if __name__ == '__main__':

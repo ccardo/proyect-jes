8a53f2cbaf3501dcc8ef16971ed659d3cfa29d07
ccardo <riccardo.gallo.pb+github@gmail.com>
2023-01-15 14:02:19 +0100

    changed the thing curato.readlines

--- Exam exercises/Obsolete words/Obsolete_words.py
+++ Exam exercises/Obsolete words/Obsolete_words.py
@@ -7,22 +7,30 @@ def main():
     with open("obsolete.txt") as obsolete_file:
         lines = [i.split() for i in obsolete_file]
     
-    word_sub = {line[0]: line[1] for line in lines}
+    word_sub = {f" {line[0]}": line[1] for line in lines}
 
     with open("text.txt") as file_text:
         text = file_text.readlines()
-    
-    for index, line in enumerate(text):
-        
-        if any(word in word_sub for word in line):
-            
-            for obsolete_word in word_sub:
-                
-                text[index] = line.replace(obsolete_word, word_sub[obsolete_word])
-    
-    with open("output.txt", "w") as output:
 
+    sub_count = {}
+    for idx, line in enumerate(text):
+        for word in word_sub:
+
+            if word not in line:
+                continue
+            count = line.count(word)
+
+            if word not in sub_count:
+                sub_count.update({word: 1})
+            else:
+                sub_count[word] += count
+            text[idx] = line.replace(word, word_sub[word], count)
+    
+    print(sub_count)
+            
+    with open("output.txt", "w") as out_file:
         for line in text:
-            output.write(line)
+            out_file.write(line)
+    
 
 main()

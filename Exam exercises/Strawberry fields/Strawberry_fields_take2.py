425f2338a80386bfd9e7d6356b230257434872d2
ccardo <riccardo.gallo.pb+github@gmail.com>
2023-02-19 15:19:37 +0100

    i diidddd

--- Exam exercises/Strawberry fields/Strawberry_fields_take2.py
+++ Exam exercises/Strawberry fields/Strawberry_fields_take2.py
@@ -7,8 +7,8 @@ def main():
         text = [i.strip(PUNCTUATION) for i in file.read().upper().split()]
 
     for i, word in enumerate(text[:-2]):
-        if len(word) == len(text[i+1]) == len(text[i+2]):
-            print(word, text[i+1], text[i+2])
+        if len(word) == len(one_ahead:=text[i+1]) == len(two_ahead:=text[i+2]):
+            print(word, one_ahead, two_ahead)
 
 if __name__ == '__main__':
     main()
